e06f38f8cdf864cae96eb3f3c43d55369196e4e6
Shibby92 <haris_000@hotmail.com>
2014-11-17 15:47:12 +0100

    Pogresno mjesto koda

--- src/JMBG.java
+++ src/JMBG.java
@@ -21,10 +21,6 @@ public class JMBG {
 		System.out.println("Za musko je rezultat -1, za zensko 1: "
 				+ provjeraSpola.compareTo(gender));
 		System.out.println("Regija:  \t\t" + jmbg.substring(7, 9));
-		String pali = "aBcDe";
-		System.out.println(pali + pali.substring(4, 5) + pali.substring(3, 4)
-				+ pali.substring(2, 3) + pali.substring(1, 2)
-				+ pali.substring(0, 1));
 		char A = jmbg.charAt(0);
 		char B = jmbg.charAt(1);
 		char C = jmbg.charAt(2);
